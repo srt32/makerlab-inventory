79734e516b580c00a5da557d259633e0a4a21133
Lauren Mo <lauren.mosenthal@gmail.com>
2014-06-16 11:21:17 -0700

    Add s3 url and path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,8 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
   :storage => :s3,
+  :url => ":s3_domain_url",
+  :path => ":class/:id.:style.:extension",
   :s3_credentials => {
     :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
