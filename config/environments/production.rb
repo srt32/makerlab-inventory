0785bbccfa70fac40d8f1f766a88b8723b245efe
Lauren Mo <lauren.mosenthal@gmail.com>
2014-06-16 10:09:37 -0700

    Comment out production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = ENV.fetch('ASSETS_VERSION')
+  # config.assets.version = ENV.fetch('ASSETS_VERSION')
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
