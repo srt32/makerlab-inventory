b1b283331ba70bee51df6328e1c952ca9e263341
Lauren Mo <lauren.mosenthal@gmail.com>
2014-06-16 11:08:26 -0700

    Update ENV variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,4 +98,14 @@ Rails.application.configure do
       secret_access_key: "zVmMGjzOuZe7kzUq++9zMgGcfRmuNuX/lhO8uGW5"
     }
   }
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :url => ":s3_domain_url",
+  :path => ":class/:id.:style.:extension",
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
